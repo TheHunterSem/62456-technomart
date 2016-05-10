15c639278d5a8f827ce1c02a8fcbed547253cde6
Vasyl <kiriljuk.vasja@gmail.com>
2016-05-10 13:11:49 +0300

    редактировал скрипт

--- js/script.js
+++ js/script.js
@@ -1,14 +1,12 @@
 var link = document.querySelector(".write-to-us");
 var writetous = document.querySelector(".write-to-us-block");
+var closeicon = document.querySelector(".write-to-us-close");
+var closewritetous = document.querySelector(".write-to-us-block");
 
 link.addEventListener("click", function(event) {
 	event.preventDefault();
 	writetous.classList.add("write-to-us-show");
 });
-
-var closeicon = document.querySelector(".write-to-us-close");
-var closewritetous = document.querySelector(".write-to-us-block");
-
 closeicon.addEventListener("click", function(event) {
 	event.preventDefault();
 	closewritetous.classList.remove("write-to-us-show");
